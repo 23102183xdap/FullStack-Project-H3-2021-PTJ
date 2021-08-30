75eb60786047192f891eca7b6dfb9585db1c77c6
SyrupSniffer <69295837+SyrupSniffer@users.noreply.github.com>
2021-08-30 08:21:46 +0200

    Category controller done

--- API/CDMRS/Controllers/CategoryController.cs
+++ API/CDMRS/Controllers/CategoryController.cs
@@ -16,6 +16,129 @@ namespace API.CDMRS.Controllers
     {
         private readonly ICategoryService _categoryService;
 
-        public 
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var categories = await _categoryService.GetAll();
+
+                if (categories == null)
+                {
+                    return Problem("Unexpected null");
+                }
+                else if (categories.Count == 0)
+                {
+                    return NoContent();
+                }
+                else
+                {
+                    return Ok(categories);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Tag))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+            try
+            {
+                var categories = await _categoryService.Get(id);
+
+                if (categories == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(categories);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Create(CategoryModel category)
+        {
+            try
+            {
+                if (category == null)
+                {
+                    return BadRequest("Cateogry fail");
+                }
+
+                var newCategory = await _categoryService.Create(category);
+
+                return Ok(newCategory);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] CategoryModel category)
+        {
+            try
+            {
+                var editCategory = await _categoryService.Update(id, category);
+
+                if (editCategory == null)
+                {
+                    return Problem("Edit failed");
+                }
+                else
+                {
+                    return Ok(editCategory);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            try
+            {
+                var category = await _categoryService.Delete(id);
+
+                return Ok(category);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
     }
 }
